fabaa8d03a60b48f4f6fc511a7ff97e5a75c36cf
moyutbc <tourokuyou888@gmail.com>
2019-09-13 22:01:12 +0900

    fix: Subject アイコンクリックでコピーに変更

--- src/views/components/subject.ts
+++ src/views/components/subject.ts
@@ -4,17 +4,17 @@ import { Clipboard } from "~/utilities/clipboard";
 
 export class Subject {
   public static setup() {
-    const subject = document.querySelector("#content h3");
-    if (!subject) {
-      return;
-    }
-    subject.onclick = () => {
-      this.copy();
-    };
-    tippy("#content h3", {
-      followCursor: true,
+    const a = document.createElement('a')
+    a.onclick = () => { this.copy(); }
+    a.className = 'icon icon-copy'
+    a.href = 'javascript:void(0);'
+    tippy(a, {
       content: "クリップボードにコピー"
     });
+
+    const subject = document.querySelector(".subject")
+    subject.style.display = 'flex'
+    subject.appendChild(a)
   }
 
   private static copy(): void {
