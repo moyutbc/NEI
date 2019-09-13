4455986a9f9480148968488f80b78acd45347fa8
moyutbc <tourokuyou888@gmail.com>
2019-09-13 22:44:49 +0900

    fix: SubtaskGanttChart グループ表示に修正

--- src/views/components/subtask-gantt-chart.ts
+++ src/views/components/subtask-gantt-chart.ts
@@ -10,22 +10,31 @@ export class SubtaskGanttChart {
     const container.id = id
 
     // タイムラインの作成
-    const data = issues.map(child => {
-      return {
-        id: child.id,
-        content: child.subject,
-        start: new Date(child.start_date || child.due_date),
-        end: child.due_date
-      }
+    const items = new DataSet()
+    const groups  = new DataSet();
+    issues.forEach((issue, index) => {
+      groups.add({ id: index, content: issue.subject });
+
+      items.add({
+        id: issue.id,
+        content: issue.subject,
+        start: new Date(issue.start_date || issue.due_date),
+        end: issue.due_date,
+        group: index
+      })
     })
-    const items = new DataSet(data)
+
     const options = {
+      groupOrder: 'content',
       stack: true,
-      type: 'box',
+      type: 'point',
       zoomMin: 1000 * 60 * 60 * 24 // msec
     }
 
-    const timeline = new Timeline(container, items, options);
+    const timeline = new Timeline(container);
+    timeline.setOptions(options);
+    timeline.setGroups(groups);
+    timeline.setItems(items);
 
     return container;
   }
