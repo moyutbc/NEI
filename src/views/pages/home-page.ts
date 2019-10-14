86942c3430102ae12e321476c789baa22a6a660f
moyutbc <tourokuyou888@gmail.com>
2019-10-14 20:31:34 +0900

    chore: hyperapp removed

--- src/views/pages/home-page.ts
+++ src/views/pages/home-page.ts
@@ -1,15 +1,4 @@
-import { h, app } from 'hyperapp'
-
 export class HomePage implements Page {
   public async create(): void {
-    app({
-      node: document.getElementById("content"),
-      view: () => h(
-        "h1", {}, [
-          "Thanks for using momiji! 😄",
-        ]
-      )
-    })
-    
   }
 }
