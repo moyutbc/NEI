2ae51afd1df7d4b028fee1bb8bd96aac92cf4577
moyutbc <tourokuyou888@gmail.com>
2019-10-14 21:04:51 +0900

    feat: Vueの導入

---
+++ src/views/components/atoms/MomijiHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2 @click="thanks">Momiji Home</h2>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+
+@Component
+export default class MomijiHeader extends Vue {
+
+  public thanks() {
+    alert('Thank you for using momiji!🍁');
+  }
+}
+</script>
--- src/views/pages/home-page.ts
+++ src/views/pages/home-page.ts
@@ -1,4 +1,7 @@
+import MomijiHeader from '~/views/components/atoms/MomijiHeader'
+
 export class HomePage implements Page {
   public async create(): void {
+    new MomijiHeader({el: '#content > h2'})
   }
 }
